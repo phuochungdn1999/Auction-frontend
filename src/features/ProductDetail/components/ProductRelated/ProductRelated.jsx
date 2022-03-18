e91bdd61e6b893a5927523c39f7e073b322292f0
shivannguyen <shivannguyen259999@gmail.com>
2022-03-18 11:14:40 +0700

    add

--- src/features/ProductDetail/components/ProductRelated/ProductRelated.jsx
+++ src/features/ProductDetail/components/ProductRelated/ProductRelated.jsx
@@ -1,37 +1,42 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styles from './ProductRelated.module.css';
 import { Link } from 'react-router-dom';
 import formatCash from '../../../../constants/formatPrice';
+const  BigNumber = require("bignumber.js");
+
 function ProductRelated(props) {
   const [products, setProducts] = useState([]);
-  useEffect(() => {
-    const getApi = `https://yshuynh.pythonanywhere.com/api/products`;
-    axios.get(getApi).then((response) => {
-      setProducts(response.data.results);
-    });
+  useEffect(async() => {
+    const getApi = `http://localhost:3002/auctions/related`;
+    const response = await axios.get(getApi);
+    setProducts(response.data.data.auctions);
+
+    
   }, []);
   return (
     <div className={styles}>
-      <h2 className={styles.header}>Sản phẩm liên quan</h2>
+      <h2 className={styles.header}>Related Products</h2>
       <div className={styles.grid__column10}>
         <div className={styles.home__product}>
           <div className={styles.grid__row}>
             {products.map((item) => (
               <div key={item.id} className={styles.grid__column24}>
                 <Link
-                  to={`/productDetail/${item.id}`}
+                  to={`/detail/${item.id}`}
                   className={styles.home__productitems}
                 >
                   <div
                     className={styles.home__productitemsimg}
-                    style={{ backgroundImage: `url(${item.thumbnail})` }}
+                    style={{ backgroundImage: `url(${item.imageLogo})` }}
                   ></div>
                   <h4 className={styles.home__productitemsname}>{item.name}</h4>
                   <div className={styles.home__productprice}>
-                    <span className={styles.home__productitemsprice}>
-                      {formatCash(item.sale_price.toString())} đ
-                    </span>
+                    {/* <span className={styles.home__productitemsprice}>
+                    {new BigNumber(item.highestBid).dividedBy(10**18).toString()}
+
+                    </span> */}
                     <div className={styles.btn_cart}>
                       <i className="fas fa-search"></i>
                       Details
